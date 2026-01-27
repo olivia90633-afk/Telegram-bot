a2b3370b17a4a133fb14f4a4c40da6a7cc58ef59
olivia90633-afk <olivia90633@gmail.com>
2026-01-27 16:01:43 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,17 @@
 from telegram import Update
-from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
-import os
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-BOT_TOKEN = os.getenv("8541110162:AAHgWjrvbe79wq9my1N6z2wtVL7zDWoP4AY")
+BOT_TOKEN = "PASTE_YOUR_BOT_TOKEN_HERE"
 
-async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Hello 👋 I am alive")
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "🔥 Ola AI is LIVE\n\nDo you want to start making serious money? 💼💰"
+    )
 
-app = ApplicationBuilder().token(BOT_TOKEN).build()
-app.add_handler(MessageHandler(filters.TEXT, reply))
+def main():
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
 
-app.run_polling()
+if __name__ == "__main__":
+    main()
